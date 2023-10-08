2b1a135fe91d1f0797e27b3a50daa4975b9101ba
Dingyi Zhang <2645366605@qq.com>
2023-10-08 13:28:31 +0100

    Vector homework hand in

--- minorHomework/20231004hw.py
+++ minorHomework/20231004hw.py
@@ -1,3 +1,5 @@
+import math
+
 class VectorValue: # For all "Magic Methods" check https://python-course.eu/oop/magic-methods.php .
 
     def __init__(self, x, y):
@@ -18,9 +20,19 @@ class VectorValue: # For all "Magic Methods" check https://python-course.eu/oop/
 
     def __str__(self):
         return (f"({self._x}, {self._y})")
+    
+    def __mul__(self, otherVector):
+        return (VectorValue((self._x * otherVector._x), (self._y * otherVector._y)))
+    
+    def __rmul__(self, otherVector):
+        return (VectorValue((self._x * otherVector._x), (self._y * otherVector._y)))
+    
+    def magnitude(self):
+        return (math.sqrt((self._x ** 2) + (self._y ** 2)))
 
 vector0 = VectorValue(2, 2)
 vector1 = VectorValue(1, 3)
 vector2 = vector0 + vector1
 vector3 = vector0 - vector1
-print(f"{vector2}\n{vector3}")
+vector4 = vector2 * vector3
+print(f"{vector2}\n{vector3}\n{vector4}")
